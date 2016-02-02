dbefbc27d03e88e87962b151cb36b9ac54c8ce1d
Kir Shatrov <shatrov@me.com>
2016-02-02 18:16:26 +0300

    WIP

--- cap_extension.rb
+++ cap_extension.rb
@@ -2,44 +2,54 @@ require 'thread'
 require 'sshkit'
 require 'mutex_m'
 
-class ShopifyRunner < SSHKit::Runner::Abstract
-  module FailedHosts
-    extend Mutex_m
-    extend self
+module FailedHosts
+  extend Mutex_m
+  extend self
 
-    def push(host)
-      instance << host
-      on_new_host(host)
+  def push(host)
+    return false
+    max_servers_to_fail = 2
+    if hosts.size > max_servers_to_fail
+      # PROBLEM: Capistrano::Configuration.servers is private here
+      # which we need to calculate failure_tolerance 0.05
+      return false
     end
 
-    def include?(host)
-      instance.include?(host)
-    end
+    hosts << host
+    on_new_host(host)
+    true
+  end
 
-    def instance
-      mu_synchronize do
-        @instance ||= Set.new
-      end
-    end
+  def include?(host)
+    instance.include?(host)
+  end
 
-    def on_new_host(host)
-      # drop it
+  def hosts
+    mu_synchronize do
+      @hosts ||= Set.new
     end
   end
 
+  def on_new_host(host)
+    # drop it
+  end
+end
+
+class ShopifyRunner < SSHKit::Runner::Abstract
   def execute
     threads = []
     hosts.each do |host|
       threads << Thread.new(host) do |h|
-        if FailedHosts.include?(h)
-          puts "ignoring #{h}"
-        else
-          begin
-            backend(h, &block).run
-          rescue StandardError, Errno::ETIMEDOUT, Net::SSH::ConnectionTimeout => e
-            b = SSHKit.config.backend.new(h)
-            b.error "Exception while executing #{host.user ? "as #{host.user}@" : "on host "}#{host}: #{e.message}"
-            FailedHosts.push(h)
+        begin
+          backend(h, &block).run
+        rescue StandardError, Errno::ETIMEDOUT, Net::SSH::ConnectionTimeout => e
+          b = SSHKit.config.backend.new(h)
+          b.error "Exception while executing #{host.user ? "as #{host.user}@" : "on host "}#{host}: #{e.message}"
+
+          # returns false if there are too many failed servers
+          # raise back
+          unless FailedHosts.push(h)
+            raise
           end
         end
       end
@@ -49,3 +59,13 @@ class ShopifyRunner < SSHKit::Runner::Abstract
 end
 
 SSHKit.config.default_runner = ShopifyRunner
+
+module MyDSL
+  def on(hosts, options={}, &block)
+    # exclude failed hosts
+    hosts = hosts - FailedHosts.hosts.to_a
+    super(hosts, options, &block)
+  end
+end
+
+self.extend MyDSL
